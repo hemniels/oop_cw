0d9d0de1406c1fcc9f0280a93a8c7579d07ee1f0
hemniels <89600300+hemniels@users.noreply.github.com>
2022-06-13 17:05:52 +0200

    gps assignment update - added IO function for Waypoints

--- ListHelp.java
+++ ListHelp.java
@@ -9,10 +9,34 @@ public class ListHelp{
 			
 	}
 	
+	
+	void IOWaypoint(double xpos, double ypos, int timestamp){
+
+			System.out.println("Geben Sie einen Wegpunkt(Xpos/Ypos/Timestamp) ein");
+			
+			System.out.println("ENTER XPOS");
+			xpos = In.readDouble();
+			
+			System.out.println("ENTER YPOS");
+			ypos = In.readDouble();
+			
+			System.out.println("ENTER TIMESTAMP");
+			timestamp = In.readInt();
+			
+			System.out.println(xpos + " " + ypos + " " + timestamp);
+			System.out.println(xpos);
+			System.out.println(ypos);
+			System.out.println(timestamp);
+	}
+	
+	
 
 
 	public static void main(String[] args){
 		
+		double DEFAULT_XPOS = 0;
+		double DEFAULT_YPOS = 0;
+		int DEFAULT_TIMESTAMP = 0;
 				
 		/* PRINTS + IO ??
 
@@ -26,14 +50,8 @@ public class ListHelp{
 		ArrayList<TestList> testArray = new ArrayList<TestList>();
 		ListHelp ListHelp = new ListHelp();
 		
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
-		ListHelp.addElem(testArray, 15.2, 13.2, 15);
+		ListHelp.IOWaypoint(DEFAULT_XPOS, DEFAULT_YPOS, DEFAULT_TIMESTAMP);
+		
 		
 		
 		for(int i = 0; i < testArray.size(); i++){
