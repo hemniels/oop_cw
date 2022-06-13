2cbafb3b1e6841b62959f849b52777738b628510
hemniels <89600300+hemniels@users.noreply.github.com>
2022-06-13 15:35:09 +0200

    gps assignment update

--- ListHelp.java
+++ ListHelp.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 
 public class ListHelp{
+	ArrayList<TestList> initialArray = new ArrayList<TestList>();
 	
-	static addElem(double x_value, double y_value, int time){}
-	
-	static createWaypointList<ArrayList<Waypoint>>;
+	void addElem(double x_value, double y_value, int time){
+		
+		initialArray.add(new TestList(x_value, y_value, time));
+	}
 	
 
 
@@ -18,6 +20,19 @@ public class ListHelp{
 		++ Kartenskalierung ist 1 = 0,1km
 		
 		*/
+		ArrayList<TestList> testArray = new ArrayList<TestList>();
+		
+		testArray.add(new TestList(2.3,3.4,15));
+		testArray.add(new TestList(3.4,3.5,12));
+		testArray.add(new TestList(122.3,3.6,123));
+		
+		for(int i = 0; i < testArray.size(); i++){
+			
+			TestList test = testArray.get(i);
+			
+			System.out.println(test.x_value + " " + test.y_value + " " + test.time);
+			
+		}
 		
 		
 	} 
