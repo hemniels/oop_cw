5b2e21197877f3ce60986c632181dd7c4ae5bbcd
hemniels <89600300+hemniels@users.noreply.github.com>
2022-06-14 02:03:56 +0200

    gps assignment update

--- ListHelp.java
+++ ListHelp.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 
-public class ListHelp{
-	
+public class IOTest{
 	
 	void IOWaypoint(ArrayList<TestList> list, double xpos, double ypos, int timestamp){
 
@@ -22,27 +21,31 @@ public class ListHelp{
 	
 	
 
-
 	public static void main(String[] args){
-		
 		double DEFAULT_XPOS = 0;
 		double DEFAULT_YPOS = 0;
 		int DEFAULT_TIMESTAMP = 0;
-		int TEST_SIZE = 0;
+		int DEFAULT_SIZE = 0;
+		int USER_SIZE = 0;
+		
+		int FULL_DISTANCE = 0;
+		double AVERAGE_SPEED = 0;
 		
 		ArrayList<TestList> DEFAULT_ARRAY = new ArrayList<TestList>();
 		
 
 		
 		
-		ListHelp ListHelp = new ListHelp();
-
-		while( TEST_SIZE < 10){
-			ListHelp.IOWaypoint(DEFAULT_ARRAY, DEFAULT_XPOS, DEFAULT_YPOS, DEFAULT_TIMESTAMP);
-			
-			TEST_SIZE++;
-		}
+		IOTest IOTest = new IOTest();
 		
+		System.out.println("Anzahl der Wegpunkte eingeben: ");
+		USER_SIZE = In.readInt();
+
+		while(DEFAULT_SIZE < USER_SIZE){
+			IOTest.IOWaypoint(DEFAULT_ARRAY, DEFAULT_XPOS, DEFAULT_YPOS, DEFAULT_TIMESTAMP);
+			
+			DEFAULT_SIZE++;
+		}
 		
 		
 		for(int i = 0; i < DEFAULT_ARRAY.size(); i++){
@@ -52,11 +55,16 @@ public class ListHelp{
 			System.out.println(test.x_value + " " + test.y_value + " " + test.time);
 			
 		}
-		
-		
-		
 	}
-	
-}
+} 
 
+				
+		/* PRINTS + IO ??
 
+		++ Gesamtstrecke zurueckgelegt
+		++ Durchschnittsgeschwindigkeit pro Stunde
+		
+		++ Kartenskalierung ist 1 = 0,1km
+		
+		*/
+		
