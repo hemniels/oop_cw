62ce64727959e1ce721280cec8c9866b67648428
hemniels <89600300+hemniels@users.noreply.github.com>
2022-07-04 20:23:51 +0200

    initialized buttons and display

--- android_calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ android_calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.b1);
+        Button b2 = findViewById(R.id.b2);
+        Button b3 = findViewById(R.id.b3);
+        Button b4 = findViewById(R.id.b4);
+        Button b5 = findViewById(R.id.b5);
+        Button b6 = findViewById(R.id.b6);
+        Button b7 = findViewById(R.id.b7);
+        Button b8 = findViewById(R.id.b8);
+        Button b9 = findViewById(R.id.b9);
+        Button b0 = findViewById(R.id.b0);
+
+        Button bAdd = findViewById(R.id.add);
+        Button bMult = findViewById(R.id.multiply);
+        Button bCE = findViewById(R.id.CE);
+        Button bDiv = findViewById(R.id.div);
+        Button bEQ = findViewById(R.id.equals);
+        Button bSub = findViewById(R.id.sub);
+
+        TextView display = findViewById(R.id.display);
+
+
+
+
     }
 }
